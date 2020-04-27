2f6148d835703dd022aa92a46938e836256c478d
akeemseymens <812@holbertonschool.com>
2020-04-27 06:29:04 +0000

    pydocstyle

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''that calculates \sum_{i=1}^{n} i^2:'''
+"""Calculate sum_{i=1}^{n} i^2."""
 
 
 def summation_i_squared(n):
-    """function that calculates summation_i_squared"""
+    """Calculate summation_i_squared."""
     if isinstance(n, int) and n > 0:
         # return sum([i**2 for i in range(1, n + 1)])
         return int((n / 6) * (n + 1) * (2 * n + 1))
