364f5b4ebb33c953a012a36125c0c6efa5ea14b7
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-04-26 23:16:02 -0700

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,4 +3,8 @@
 
 
 def summation_i_squared(n):
-   return sum(i**2 for i in range(1, n+1))
+    """function that calculates summation_i_squared"""
+    if isinstance(n, int) and n > 0:
+        # return sum([i**2 for i in range(1, n + 1)])
+        return int((n / 6) * (n + 1) * (2 * n + 1))
+    return None
