742801b69b9f0613fedd6cf2677b428173f611a8
akeemseymens <812@holbertonschool.com>
2020-05-01 17:49:36 +0000

    added bug fixes

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -5,9 +5,11 @@
 class Exponential:
     """Intializig the class."""
 
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """Initialize exponential distribution."""
-        if data is None:
+        if data is None and isinstance(lambtha, (float, int)):
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
@@ -16,16 +18,16 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = len(data) / sum(data)
+            self.lambtha = float(1 / (sum(data) / len(data)))
 
     def pdf(self, x):
         """Calculate the probability density given time period x."""
         if x is None or x < 0:
             return 0
-        return (self.lambtha * pow(2.7182818285, -1 * self.lambtha * x))
+        return (self.lambtha * pow(Exponential.e, (-1 * self.lambtha) * x))
 
     def cdf(self, x):
         """Calculate cumulative distribution for a given time period x."""
         if x is None or x < 0:
             return 0
-        return (self.lambtha * pow(2.7182818285, -1 * self.lambtha * x))
+        return (1 -(pow(Exponential.e , (-1 * self.lambtha) * x)))
