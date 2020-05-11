c36d724796fa9672e4356ccd6f8338f91bbcb57b
akeemseymens <812@holbertonschool.com>
2020-05-11 05:42:25 +0000

    formatting issue

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-"""Class for Neuron."""
+"""
+Class Neuron, defines a single neuron performing binary classification
+"""
 import numpy as np
 
 
 class Neuron:
-    """Defines a single neuron performing binary classifications."""
-
+    """ Define a single neuron performing binary classification"""
     def __init__(self, nx):
-        """Intialize the class."""
-        if type(nx) is not int:
-            raise TypeError("nx must be a integer")
+        """Define class Neuron"""
+        self.nx = nx
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
