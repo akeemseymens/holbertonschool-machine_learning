e5670bd3ed1e2e3febd9d40119696892f40da8cf
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-05-20 20:52:41 -0700

    Update 7-evaluate.py
    change file loader function call.

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -7,12 +7,12 @@ import tensorflow as tf
 def evaluate(X, Y, save_path):
     """Evaluate the output of a neural network"""
     with tf.Session() as sess:
-        save_file = tf.train.import_meta_graph('save_path/model.ckpt.meta')
+        save_file = tf.train.import_meta_graph(save_path + '.meta')
         save_file.restore(sess, save_path)
-        var_names = ['x', 'y', 'y_pred', 'accuracy', 'loss']
+        var_names = ['x', 'y', 'pred', 'accuracy', 'loss']
         for var_name in var_names:
             globals()[var_name] = tf.get_collection(var_name)[0]
-        y_pred = sess.run(globals()['y_pred'], feed_dict={x: X, y: Y})
+        pred = sess.run(globals()['pred'], feed_dict={x: X, y: Y})
         loss = sess.run(globals()['loss'], feed_dict={x: X, y: Y})
         acc = sess.run(globals()['accuracy'], feed_dict={x: X, y: Y})
-    return y_pred, acc, loss
+    return pred, acc, loss
