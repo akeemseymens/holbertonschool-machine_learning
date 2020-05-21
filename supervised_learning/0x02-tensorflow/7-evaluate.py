87a0730f0beb52d3e728cfee08961765d164d8c6
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-05-20 20:54:49 -0700

    Update 7-evaluate.py
    bug fix

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-"""Evaluates the output of a neural network"""
-
+"""
+Evaluate model of neural network.
+"""
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """Evaluate the output of a neural network"""
+    """function that evaluates the output of a neural network"""
     with tf.Session() as sess:
-        save_file = tf.train.import_meta_graph(save_path + '.meta')
-        save_file.restore(sess, save_path)
-        var_names = ['x', 'y', 'pred', 'accuracy', 'loss']
+        loader = tf.train.import_meta_graph(save_path + '.meta')
+        loader.restore(sess, save_path)
+        var_names = ['x', 'y', 'y_pred', 'accuracy', 'loss']
         for var_name in var_names:
             globals()[var_name] = tf.get_collection(var_name)[0]
-        pred = sess.run(globals()['pred'], feed_dict={x: X, y: Y})
+        y_pred = sess.run(globals()['y_pred'], feed_dict={x: X, y: Y})
         loss = sess.run(globals()['loss'], feed_dict={x: X, y: Y})
         acc = sess.run(globals()['accuracy'], feed_dict={x: X, y: Y})
-    return pred, acc, loss
+    return y_pred, acc, loss
